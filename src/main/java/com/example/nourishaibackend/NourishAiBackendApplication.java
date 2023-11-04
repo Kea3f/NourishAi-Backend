9220eb49073d66349b0f4ab23d2f53a5952021f5
Najamoe <naja0718@stud.kea.dk>
2023-11-04 18:12:03 +0100

    Initial

---
+++ src/main/java/com/example/nourishaibackend/NourishAiBackendApplication.java
@@ -0,0 +1,13 @@
+package com.example.nourishaibackend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NourishAiBackendApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(NourishAiBackendApplication.class, args);
+    }
+
+}
